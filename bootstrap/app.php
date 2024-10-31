93c964651542dbacafa709ef295f70dcd3f7ee86
Viet VU <jooservices@gmail.com>
2024-10-31 17:50:32 +0700

    Sentry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,13 +6,15 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        if (app()->bound('sentry')) {
+            app('sentry')->captureException($exceptions);
+        }
     })->create();
