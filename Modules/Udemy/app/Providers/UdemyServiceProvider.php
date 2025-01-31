e83baf9d963cdec63640ea0629f397d1b1a9d3fb
Viet VU <jooservices@gmail.com>
2025-01-31 13:35:54 +0700

    Remove views register

--- Modules/Udemy/app/Providers/UdemyServiceProvider.php
+++ Modules/Udemy/app/Providers/UdemyServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Modules\Udemy\Providers;
 
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use Modules\Udemy\Console\StudyCourse;
@@ -29,7 +28,6 @@ class UdemyServiceProvider extends ServiceProvider
         $this->registerCommandSchedules();
         $this->registerTranslations();
         $this->registerConfig();
-        $this->registerViews();
         $this->loadMigrationsFrom(module_path($this->name, 'database/migrations'));
     }
 
@@ -91,22 +89,6 @@ class UdemyServiceProvider extends ServiceProvider
     {
         $this->publishes([module_path($this->name, 'config/config.php') => config_path($this->nameLower.'.php')], 'config');
         $this->mergeConfigFrom(module_path($this->name, 'config/config.php'), $this->nameLower);
-    }
-
-    /**
-     * Register views.
-     */
-    public function registerViews(): void
-    {
-        $viewPath = resource_path('views/modules/'.$this->nameLower);
-        $sourcePath = module_path($this->name, 'resources/views');
-
-        $this->publishes([$sourcePath => $viewPath], ['views', $this->nameLower.'-module-views']);
-
-        $this->loadViewsFrom(array_merge($this->getPublishableViewPaths(), [$sourcePath]), $this->nameLower);
-
-        $componentNamespace = $this->module_namespace($this->name, $this->app_path(config('modules.paths.generator.component-class.path')));
-        Blade::componentNamespace($componentNamespace, $this->nameLower);
     }
 
     /**
